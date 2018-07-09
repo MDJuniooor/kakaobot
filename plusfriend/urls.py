274533afdab2e74807e37108c9b2be2f6db1ae82
DaeSung <anseotjd12@gmail.com>
2018-07-09 16:18:52 +0900

    카카오톡 플러스친구 기본 API 구현

--- plusfriend/urls.py
+++ plusfriend/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
+from . import views
 app_name = 'plusfriend'
 
 urlpatterns = [
-  
+    path('keyboard', views.on_init),
+    path('friend', views.on_added),
+#    re_path(r'friend/(?P&lt;user_key&gt;[\w-]+)$', views.on_block),
+#    re_path(r'chat_room/(?P&lt;user_key&gt;[\w-]+)$', views.on_leave),
+    path('message', views.on_message),
 ]
--- plusfriend/views.py
+++ plusfriend/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from .decorators import bot
+from . import functions
 
-# Create your views here.
+
+@bot
+def on_init(request):
+    return {'type': 'text'}
+
+
+@bot
+def on_message(request):
+    user_key = request.JSON['user_key']
+    type = request.JSON['type']
+    content = request.JSON['content']  # photo 타입일 경우에는 이미지 URL
+    response = '아직 구현되지 않았습니다'
+    
+    return {
+        'message': {
+            'text': response,
+        }
+    }
+
+
+@bot
+def on_added(request):
+    user_key = request.JSON['user_key']
+
+@bot
+def on_block(request, user_key):
+    pass
+
+@bot
+def on_leave(request, user_key):
+    pass
